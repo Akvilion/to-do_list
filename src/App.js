54204b3962ed4ddbc3e186d536f9dc299e7061db
Akvilion <ianko.andrii@gmail.com>
2021-03-14 22:28:03 +0200

    to-do_list app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.maxId = 1
+
+    this.state = {
+      
+      new_item_title: '',
+
+      items: []
+    };
+  }
+
+  onDeleteItem(id){
+    const items_copy = [...this.state.items]
+    const updated_copy = items_copy.filter( item => item.id !== id )
+    this.setState({ items: updated_copy })
+  }
+
+  onNewItemTitleChange(value){
+    this.setState({ new_item_title: value })
+  }
+
+  onAddItem(){
+    const items_copy = [...this.state.items]
+    const newItem = {
+      id: this.maxId + 1,
+      title: this.state.new_item_title
+    }
+    this.maxId++
+
+    items_copy.push( newItem )
+    this.setState({ items: items_copy, new_item_title: '' })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={( e ) => this.onNewItemTitleChange( e.target.value )}
+        value = { this.state.new_item_title } type="text" size="25" placeholder="Type element here..."/> <button onClick= {() => this.onAddItem()} >ADD</button>
+        
+        
+        <ul>{this.state.items.map( elem => <li> {elem.title} <button class="deleteBTN" onClick= { () => this.onDeleteItem(elem.id)  } ><b>X</b></button> </li>)}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
